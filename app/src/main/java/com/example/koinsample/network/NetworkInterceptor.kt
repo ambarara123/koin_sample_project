8f7fb8c355b909effa852ba69757e4a737640b06
ambarkumarpandey <ambar.ara123@gmail.com>
2020-01-17 17:07:20 +0530

    Interceptor class added

--- app/src/main/java/com/example/koinsample/network/NetworkInterceptor.kt
+++ app/src/main/java/com/example/koinsample/network/NetworkInterceptor.kt
@@ -1,1 +1,20 @@
 package com.example.koinsample.network
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class NetworkInterceptor() : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+
+        var req = chain.request()
+        val url = req.url()
+            .newBuilder()
+            .addQueryParameter("APPID", "your_api_key")
+            .build()
+
+        req = req.newBuilder().url(url).build()
+
+        return chain.proceed(req)
+    }
+
+}
